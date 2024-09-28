86976d6ad4d040d323ec29bff3fa7b0d94ac01b3
SashaNaidanov <naidanov.prod@yandex.ru>
2024-09-28 20:51:01 +0300

    Сreate a Car entity, set up a One-to-One Relationship, check the logic for adding data to the database, implement a method using a HQL-query

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -26,4 +26,12 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public List<User> getUserByModelAndSeries(String model, int series) {
+      return sessionFactory.getCurrentSession()
+              .createQuery("from User where car.model = :model and car.series = :series", User.class)
+              .setParameter("model", model)
+              .setParameter("series", series)
+              .getResultList();
+   }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 public class User {
 
    @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
    @Column(name = "name")
@@ -18,6 +17,11 @@ public class User {
 
    @Column(name = "email")
    private String email;
+
+   @OneToOne
+   @MapsId
+   @JoinColumn(name = "id")
+   private Car car;
 
    public User() {}
    
@@ -57,5 +61,22 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              '}';
    }
 }
